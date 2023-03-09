7c4b054b3515678e353700a8da36d53dd43ca7c9
SaraLund <masaia121@gmail.com>
2023-03-09 12:54:28 +0100

    Added tutorial project

---
+++ GeeksTutorial/src/main/java/com/example/geekstutorial/GeeksTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.geekstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeksTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeeksTutorialApplication.class, args);
+	}
+
+}
---
+++ GeeksTutorial/src/test/java/com/example/geekstutorial/GeeksTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geekstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeksTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
